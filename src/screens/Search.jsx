52f56205008c0f90fa90e725ead17c088dad1b07
camilla <camillaalves285@gmail.com>
2024-01-21 19:22:16 -0300

    Componente de busca criado

--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -1,75 +1,99 @@
-
 import React from 'react';
-import {Text, View, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-const styleInput = () =>  {
-   return{
-       outlineStyle: 'none'
- 
-     }
- }
+//validação dos campos
+const schema = yup.object({
+   description: yup.string().required("Informe a descrição do item")
+})
 
- export default function Search({title})  {
-   return(
-      <View style={styles.div}>
-      <Text style={styles.h1}>{title}</Text>
-  
-      <View style={styles.divInputs}>
-        <View style={[styles.inputs, {gap: '1.2rem'}]}>
-          <View style={styles.inputs}>
-            <Text style={styles.textInput}>Procurar item:</Text>
-            <TextInput style={[styles.input, styleInput()]} />
-          </View>
-          <TouchableOpacity style={styles.button} activeOpacity={0.7}  >
-            <Text style={styles.textButton}>Enviar</Text>
-          </TouchableOpacity>
-  
-        </View>
+export default function Search() {
+
+   const {control, handleSubmit, formState: { errors } }  = useForm({
+      resolver: yupResolver(schema)
+   })
+
+   function handleSignIn(data) {
+      console.log(data)
+   }
+
+   const styleInput = () =>  {
+      return{
+         outlineStyle: 'none'
+      }
+   }
+
+ return(
+      <View style={styles.container}>
+         <Text style={styles.title}>Procurar Item</Text>
+
+         <Controller 
+            control={control}
+            name='description'
+            render={({ field: { onChange, onBlur, value } }) => (
+               <TextInput 
+               style={[styles.input, {borderWidth: errors.description && 1, borderColor: errors.description && "#ff375b"}, styleInput()]}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  placeholder='Descrição do item'
+               />
+         )}
+         />
+         { errors.description && <Text style={styles.labelError}>{errors.description?.message}</Text>}
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={handleSubmit(handleSignIn)}>
+            <Text style={styles.buttonText}>Procurar</Text>
+         </TouchableOpacity>
+
+
       </View>
-    </View>
-    )
+   )
 }
 
 const styles = StyleSheet.create({
-   div: {
-    flex: 1,
-     justifyContent: 'center', 
-     alignItems: 'center',
-   },
-   h1: {
-     fontSize: 22,
-     marginBottom: '2rem'
-   },
-   divInputs: {
-      display: 'flex',
-      gap: '0.8rem'
-    },
-    inputs: {
-      display:'flex',
-      gap: '0.5rem',
-      width: '17rem'
-    },
-    input: {
-      border: '1px solid #000',
-      height: '2.5rem',
-      borderRadius: '0.5rem',
-      fontSize: '1.2rem',
-    },
-    textInput: {
-      fontSize: '1.2rem'
-    },
-    button: {
-      backgroundColor: '#1877F2',
+   container: {
+      flex: 1,
+      justifyContent: 'center', 
       alignItems: 'center',
-      padding: '0.5rem',
-      borderRadius: '0.5rem',
+      gap: '1rem',
+      margin: '1rem'
+   },
+   title: {
+      fontSize: 34,
+      marginBottom: 34,
+      color: '#121212',
+      fontWeight: 'bold'
+   },
+   input: {
+      width: '90%',
+      height: '7%',
+      border: '1px solid #000',
+      borderRadius: 8,
+      padding: 5,
+      fontSize: 18
+   },
+   button: {
+      width: '90%',
+      height: '7%',
+      borderRadius: 8,
+      padding: 5,
+      fontSize: 18,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#236ef8',
+   },
+   buttonText: {
       color: '#fff',
-      marginTop: '0.5rem'
-    },
-    textButton: {
-       color: '#fff',
-       fontSize: '1.2rem',
-       fontWeight: 'bold'
-     },
- });
- 
+      fontWeight: '600',
+      fontSize: 16
+   },
+   labelError: {
+      alignSelf: 'flex-start',
+      color: '#ff375b',
+      marginTop: -15,
+      marginLeft: 20
+   }
+})
